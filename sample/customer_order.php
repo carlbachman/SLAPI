34c2459feb0d1edb79b94b2010bdf544ea39b884
carlbachman <carl.bachman.kharazmi@gmail.com>
2014-06-18 12:25:38 +0800

    Handle quantity auto indexing. Clean-up

--- sample/customer_order.php
+++ sample/customer_order.php
@@ -10,29 +10,25 @@ $client = SoftLayer_SoapClient::getClient('SoftLayer_Product_Order', null, SLAPI
 $my_order = new stdClass();
 $my_order->packageId = 46;
 $my_order->location  = 224092;
-$my_order->quantity  = 1;
+$my_order->quantity  = 2;
 $my_order->imageTemplateGlobalIdentifier  = '3dd41c1d-aa8e-49f8-ae0a-57f4427d1665';
 $my_order->virtualGuests = array();
 $my_order->prices = array();
 $os_item_price = 24341; // debian 7 minimal
 
-$fqdns = array(
-         array('hostname' => 'carl4',
-               'domain' => 'softlayer-singapore-test.com',
-               'localDiskFlag' => true,
-               'hourlyBillingFlag' => true),
-              );
-foreach ($fqdns as $fqdn) {
+for ($cnt = 0; $cnt < $my_order->quantity; $cnt++) {
   $domain = new stdClass();
-  $domain->hostname = $fqdn['hostname'];
-  $domain->domain = $fqdn['domain'];
+  $domain->hostname = "domain-sg-$cnt";;
+  $domain->domain = 'softlayer-singapore-test.com';
+  $domain->localDiskFlag = true;
+  $domain->hourlyBillingFlag = true;
   $my_order->virtualGuests[] = $domain;
 }
 
 $item_price_ids = array(26125, // 1x 2Ghz
                         22694, // 27884, // 2G, 4G 22694
                         23070, // Reboot remote console
-                        24713, // Gbit pub and priv
+                        24713, // GbE pub and priv
                         27674, // 5TB data
                         34807, // 1 IP
                         24013, // 25G LOCAL
@@ -44,7 +40,7 @@ $item_price_ids = array(26125, // 1x 2Ghz
                         );
 
 if (empty($my_order->imageTemplateGlobalIdentifier))
-$item_price_ids[] = $os_item_price;
+  $item_price_ids[] = $os_item_price;
 
 foreach ($item_price_ids as $id) {
   $price = new stdClass();
@@ -68,4 +64,5 @@ if ('yes' == $argv[1]) {
   }
 }
 
+echo shell_exec('date');
 ?>
