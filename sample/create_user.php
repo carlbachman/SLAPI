4d21b597ac8ad3baeeb57b0ed48332e3795c2fd1
carlbachman <carl.bachman.kharazmi@gmail.com>
2014-08-01 04:05:31 -0500

    Initial version of guest creation sample tool

---
+++ sample/create_user.php
@@ -0,0 +1,78 @@
+<?php
+
+if (!is_file('config.ini')) {
+  echo "Oops! Could not find config.ini\n"; 
+  exit(1);
+}
+require_once 'config.ini'; 
+
+define('CREATE_NBR_ACCOUNTS', 2);
+$cnt = 0;
+$my_user = new stdClass();
+$my_user->address1 = 'myaddress';
+$my_user->city = 'Ulan Bator';
+$my_user->companyName = 'SoftLayer';
+$my_user->country = 'TH';
+$my_user->email = 'foo@bar.com';
+$my_user->firstName = 'SoftLayer';
+$my_user->lastName = 'TopGun';
+$my_user->postalCode = '12345';
+$my_user->secondaryLoginRequiredFlag = false;
+$my_user->userStatusId = 1001;
+$my_user->timezoneId = 153;
+
+$default_perm = array('USER_MANAGE', 'ACCOUNT_SUMMARY_VIEW',
+                      'RESET_PORTAL_PASSWORD', 'SERVER_CANCEL', 'NTF_SUBSCRIBER_MANAGE',
+                      'REMOTE_MANAGEMENT', 'TICKET_VIEW', 'TICKET_VIEW_ALL', 'TICKET_SEARCH', 'TICKET_ADD',
+                      'TICKET_EDIT', 'FIREWALL_MANAGE', 'ANTI_MALWARE_MANAGE', 'NETWORK_IDS_MANAGE',
+                      'SECURITY_CERTIFICATE_VIEW', 'GATEWAY_MANAGE', 'HOST_ID_MANAGE',
+                      'VULN_SCAN_MANAGE', 'SECURITY_CERTIFICATE_MANAGE', 'CUSTOMER_SSH_KEY_MANAGEMENT',
+                      'HARDWARE_VIEW', 'REMOTE_MANAGEMENT', 'MONITORING_MANAGE', 'SERVER_RELOAD',
+                      'SERVER_RELOAD', 'HOSTNAME_EDIT', 'VIRTUAL_GUEST_VIEW', 'REMOTE_MANAGEMENT',
+                      'MONITORING_MANAGE', 'PUBLIC_IMAGE_MANAGE', 'SERVER_RELOAD', 'SERVER_RELOAD',
+                      'HOSTNAME_EDIT', 'LICENSE_VIEW', 'VIEW_CPANEL', 'VIEW_PLESK', 'VIEW_HELM',
+                      'VIEW_QUANTASTOR', 'BANDWIDTH_MANAGE', 'PORT_CONTROL', 'DNS_MANAGE', 'DNS_MANAGE',
+                      'DNS_MANAGE', 'DNS_MANAGE', 'LOADBALANCER_MANAGE', 'CDN_ACCOUNT_MANAGE',
+                      'CDN_FILE_MANAGE', 'CDN_BANDWIDTH_VIEW', 'NETWORK_ROUTE_MANAGE',
+                      'FIREWALL_RULE_MANAGE', 'BANDWIDTH_MANAGE', 'PORT_CONTROL', 'LOCKBOX_MANAGE',
+                      'NETWORK_TUNNEL_MANAGE', 'NAS_MANAGE', 'SERVER_RELOAD', 'SERVER_RELOAD',
+                      'NETWORK_VLAN_SPANNING', 'VPN_MANAGE', 'SERVER_ADD', 'SERVER_UPGRADE', 'SERVICE_ADD',
+                      'INSTANCE_UPGRADE', 'IP_ADD', 'ADD_SERVICE_STORAGE', 'SERVER_CANCEL',
+                      'SERVICE_CANCEL');
+
+function get_word($len = 8) {
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    for ($i = 0; $i < $len; $i++) {
+      $word .= $chars{mt_rand(0, strlen($chars) - 1)};
+    }
+    return $word;
+}
+
+foreach ($default_perm as $value) {
+  ${'my_perm' . $cnt}['keyName'] = $value;
+  $perm[] = ${'my_perm' . $cnt};
+  $cnt++;
+}
+
+for ($user = 0; $user < CREATE_NBR_ACCOUNTS; $user++) {
+  $my_user->username = 'carl.testE' . $user;
+  $my_word = get_word();
+  $client = SoftLayer_SoapClient::getClient('SoftLayer_User_Customer', '', SLAPI_USER, SLAPI_KEY);
+
+  try {
+    $res = $client->createObject($my_user, "!$my_word", "!$my_word");
+  } catch (Exception $e) {
+    die('Oops! Something went wrong: ' . $e->getMessage() . "\n");
+  }
+
+  $client = SoftLayer_SoapClient::getClient('SoftLayer_User_Customer', $res->id, SLAPI_USER, SLAPI_KEY);
+
+  try {
+    $res = $client->addBulkPortalPermission($perm);
+  } catch (Exception $e) {
+    die('Oops! Something went wrong: ' . $e->getMessage() . "\n");
+  }
+
+  echo "$user : Credentials $my_user->username $my_word\n";
+}
+?>
