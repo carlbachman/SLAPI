1f6265e82a68741ecdbd19ae9ea3bd9352f80699
carlbachman <carl.bachman.kharazmi@gmail.com>
2015-07-18 12:17:22 +0800

    Correct alignment and use print routines

--- sample/helpers/list_user_consumption.php
+++ sample/helpers/list_user_consumption.php
@@ -7,22 +7,24 @@ if (!is_file('config.ini')) {
 require_once 'config.ini'; 
 
 
-function print_item($item)
+function print_user($user)
 {
-  $user = $item->billingItem->orderItem->order->userRecord->username; 
-  $user = strlen($user) < 8 ? "$user    " : $user;
-  $code = $item->billingItem->categoryCode; 
-  $code = strlen($code) < 8 ? "$code    " : $code;
-echo $item->billingItem->createDate . "\t" . 
-     $code . "\t" .
-     $item->billingItem->orderItem->recurringFee . "\t\t\t" .
-     $user . "\t" .
-     $item->fullyQualifiedDomainName . "\n";
+  foreach ($user as $device) {
+    $recurring = !empty($device->billingItem->orderItem->recurringFee) ? 
+                 $device->billingItem->orderItem->recurringFee : 
+                 $device->billingItem->orderItem->hourlyRecurringFee;
+    printf("%s\t%-10s\t%-10s\t%-20s\t%s\n", $device->billingItem->createDate,
+                                         $device->billingItem->categoryCode,
+                                         $recurring,
+                                         $device->billingItem->orderItem->order->userRecord->username,
+                                         $device->fullyQualifiedDomainName);
+  }
 }
 
 $client = SoftLayer_SoapClient::getClient('SoftLayer_Account', null, SLAPI_USER, SLAPI_KEY);
 $object_mask = "mask[fullyQualifiedDomainName, billingItem[categoryCode,description,createDate,orderItem[order[userRecord[username]]]]]";
 $client->setObjectMask($object_mask);
+$result = array();
 
 try {
   $cci_list = $client->getVirtualGuests();
@@ -31,12 +33,18 @@ try {
   die('Oops! Something went wrong: ' . $e->getMessage() . "\n");
 }
 
-echo "Create date\t\t\tCategory code\tRecurring fee (USD)\tUsername\tFQDN\n\n";
+printf("%s\t\t\t%s\t%s\t%s\t\t%s\n", 'Create date', 'Category code', 'Recurring', 'Username', 'FQDN');
 
 foreach($cci_list as $c) {
-  print_item($c);
+  $result[$c->billingItem->orderItem->order->userRecord->username][] = $c;
 }
+
 foreach($bm_list as $b) {
-  print_item($b);
+  $result[$b->billingItem->orderItem->order->userRecord->username][] = $b;
 }
+
+foreach ($result as $user) {
+  print_user($user);
+}
+
 ?>
